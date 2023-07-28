e7c51ff508c1ee4ccea126ce1c480911370e4fe6
Debbora Leip <leip@pik-potsdam.de>
2023-07-28 10:19:10 +0200

    added missing files

--- R/mrfactors-package.R
+++ R/mrfactors-package.R
@@ -1,4 +1,11 @@
-# The package documentation is defined in this file.
-# You can get it via `library(<package>); ?<package>`.
+#' MADRaT based package on factor inputs
+#'
+#' This package provides functions for MAgPIE input data on factor inputs to agricultural production 
+#' (with a focus on capital and labor).
+#'
+#' @name mrfactors-package
+#' @aliases mrfactors-package mrfactors
 #' @docType package
-"_PACKAGE"
+#' @author Maintainer: Debbora Leip <leip@pik-potsdam.de>
+#' @import madrat magclass mrdrivers mstools mrcommons
+NULL
